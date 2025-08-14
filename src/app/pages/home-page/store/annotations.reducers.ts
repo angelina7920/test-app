3e1b2d26f68e763af4f33c4b79423bb67ac7e75e
angelina7920 <angelina.skutskaya11@gmail.com>
2025-08-14 11:54:14 +0300

    add ngrx entity to annotations reducer

--- src/app/pages/home-page/store/annotations.reducers.ts
+++ src/app/pages/home-page/store/annotations.reducers.ts
@@ -1,13 +1,22 @@
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
 import { createFeature, createReducer, on } from '@ngrx/store';
 import { AnnotationsActions, PolygonDTO } from './annotations.actions';
 
-export interface AnnotationsState {
-  byId: Record<string, PolygonDTO[]>;
+export interface AnnotationsState extends EntityState<PolygonDTO> {
+  byImageId: Record<string, string[]>;
 }
 
-const initialState: AnnotationsState = {
-  byId: {},
-};
+export const annotationsEntityAdapter: EntityAdapter<PolygonDTO> =
+  createEntityAdapter<PolygonDTO>({
+    selectId: (polygon) => {
+      const id = `${polygon.basePoints[0]?.x}-${polygon.basePoints[0]?.y}-${polygon.angle}-${polygon.color}`;
+      return id;
+    },
+  });
+
+const initialState: AnnotationsState = annotationsEntityAdapter.getInitialState({
+  byImageId: {},
+});
 
 export const annotationsFeatureKey = 'annotations';
 
@@ -15,18 +24,35 @@ export const annotationsReducer = createFeature({
   name: annotationsFeatureKey,
   reducer: createReducer(
     initialState,
-    on(AnnotationsActions.savePolygons, (state, { imageId, polygons }) => ({
-      ...state,
-      byId: {
-        ...state.byId,
-        [imageId]: polygons,
-      },
-    })),
-    on(AnnotationsActions.clearPolygons, (state, { imageId }) => {
-      const { [imageId]: _, ...rest } = state.byId;
+    on(AnnotationsActions.savePolygons, (state, { imageId, polygons }) => {
+      const existingIds = state.byImageId[imageId] || [];
+      const newState = existingIds.length > 0 
+        ? annotationsEntityAdapter.removeMany(existingIds, state)
+        : state;
+
+      const newStateWithPolygons = annotationsEntityAdapter.addMany(polygons, newState);
+      
+      const newPolygonIds = polygons.map(polygon => 
+        annotationsEntityAdapter.selectId(polygon) as string
+      );
+
       return {
-        ...state,
-        byId: rest,
+        ...newStateWithPolygons,
+        byImageId: {
+          ...newStateWithPolygons.byImageId,
+          [imageId]: newPolygonIds,
+        },
+      };
+    }),
+    on(AnnotationsActions.clearPolygons, (state, { imageId }) => {
+      const existingIds = state.byImageId[imageId] || [];
+      if (existingIds.length === 0) return state;
+
+      const { [imageId]: _, ...restByImageId } = state.byImageId;
+      
+      return {
+        ...annotationsEntityAdapter.removeMany(existingIds, state),
+        byImageId: restByImageId,
       };
     }),
   ),
--- src/app/pages/home-page/store/annotations.selectors.ts
+++ src/app/pages/home-page/store/annotations.selectors.ts
@@ -2,15 +2,39 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import {
   annotationsFeatureKey,
   AnnotationsState,
+  annotationsEntityAdapter,
 } from './annotations.reducers';
 
 const selectAnnotationsState = createFeatureSelector<AnnotationsState>(
   annotationsFeatureKey,
 );
 
+const { selectAll, selectEntities } = annotationsEntityAdapter.getSelectors();
+
+const selectAllPolygons = createSelector(selectAnnotationsState, selectAll);
+
+const selectPolygonEntities = createSelector(selectAnnotationsState, selectEntities);
+
 const selectPolygonsByImageId = (imageId: string) =>
-  createSelector(selectAnnotationsState, (state) => state.byId[imageId] ?? []);
+  createSelector(
+    selectAnnotationsState,
+    (state) => {
+      const polygonIds = state.byImageId[imageId] || [];
+      return polygonIds
+        .map(id => state.entities[id])
+        .filter((polygon): polygon is NonNullable<typeof polygon> => polygon !== undefined);
+    }
+  );
+
+const selectPolygonCountByImageId = (imageId: string) =>
+  createSelector(
+    selectAnnotationsState,
+    (state) => state.byImageId[imageId]?.length || 0
+  );
 
 export const AnnotationsSelectors = {
+  selectAllPolygons,
+  selectPolygonEntities,
   selectPolygonsByImageId,
+  selectPolygonCountByImageId,
 };
